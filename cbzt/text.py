39aa87d59bc4ba4ce2d994d4f98cd2a0dcbba204
Per Andersson <avtobiff@gmail.com>
2009-08-13 13:14:14 +0200

    font and text rendering work.
    * cbzt/font.py
      + added space and I to font set.
    * cbzt/text.py
      + fixed font rendering.

--- cbzt/text.py
+++ cbzt/text.py
@@ -8,7 +8,7 @@ def head(list):
 def tail(list):
     return list[1:]
 
-def draw_character(surface,xpos,char,dimension,bg,fg):
+def draw_character(surface,s_xpos,char,dimension,bg,fg):
     width,height = dimension
     char_surface = pygame.Surface((cbzt.font.WIDTH*width,cbzt.font.HEIGHT*height))
     char_surface.fill(bg)
@@ -16,16 +16,16 @@ def draw_character(surface,xpos,char,dimension,bg,fg):
     char_pixel.fill(fg)
     ypos = 0
     for y in range(cbzt.font.HEIGHT):
-        ypos += height
         xpos = 0
         for x in range(cbzt.font.WIDTH):
-            xpos += width
-            if cbzt.font.font[char][x][y]:
+            if cbzt.font.font[char][y][x]:
                 char_surface.blit(char_pixel,(xpos,ypos))
-    print "drawc",char
-    return char_surface
+            xpos += width
+        ypos += height
+    surface.blit(char_surface,(s_xpos,0))
+    return surface
 
-def draw(string,bg=(0,)*3,fg=(255,)*3,width=5,height=5,surface=None,xpos=0):
+def render(string,bg=(0,)*3,fg=(255,)*3,width=5,height=5,surface=None,xpos=0):
     """
     string - the string to be rendered
     bg - background colour (default (0,0,0))
@@ -35,17 +35,17 @@ def draw(string,bg=(0,)*3,fg=(255,)*3,width=5,height=5,surface=None,xpos=0):
 
     returns a surface with the rendered string
     """
-    if len(string) <= 0:
+    if len(string) <= 0: # return final product
         return surface
-    elif surface is None:
+    elif surface is None: # create canvas
         # all character's widths plus spaces in between
-        x = len(string)*width*cbzt.font.WIDTH+(len(string)-1)*cbzt.font.WIDTH
+        x = len(string)*width*cbzt.font.WIDTH#+(len(string))*cbzt.font.WIDTH
         # character height
-        y = cbzt.font.HEIGHT
+        y = height*cbzt.font.HEIGHT
         s = pygame.Surface((x,y))
         s.fill(bg)
-        return draw(string,bg,fg,width,height,s)
-    else:
-        xpos += cbzt.font.WIDTH+1 # space plus a new character
-        draw_character(surface,xpos,head(string),(width,height),bg,fg)
-        return draw(tail(string),bg,fg,width,height,surface,xpos)
+        return render(string,bg,fg,width,height,s)
+    else: # recurse over string and draw message
+        surface = draw_character(surface,xpos,head(string),(width,height),bg,fg)
+        xpos += width*(cbzt.font.WIDTH+1) # space plus a new character
+        return render(tail(string),bg,fg,width,height,surface,xpos)
