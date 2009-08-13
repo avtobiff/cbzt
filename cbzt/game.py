e38c278515bf3c1028a65b3e813ed0ebd8a6be64
Per Andersson <avtobiff@gmail.com>
2009-08-14 01:14:04 +0200

    added Ball class.
    * cbzt/game.py (Ball): new class, represents a ball.

--- cbzt/game.py
+++ cbzt/game.py
@@ -5,6 +5,18 @@ from draw import Drawable
 from exception import EndGame
 
 
+class Ball(Drawable):
+    def __init__(self,x,y,direction,velocity):
+        self.direction = direction
+        self.velocity = velocity
+        ball = pygame.Surface((10,10))
+        ball.fill((255,)*3)
+        Drawable.__init__(self,ball,x,y)
+
+    def update(self):
+        pass
+
+
 class Board(Drawable):
     def __init__(self):
         self.build()
@@ -20,13 +32,16 @@ class Board(Drawable):
         for y in range(32):
             self.surface.blit(marker,(320,y*20+5))
 
+
 class Game(object):
-    def __init__(self,screen,p0,p1):
+    def __init__(self,screen,p0,p1,ball):
+        self.clock = pygame.time.Clock()
         self.screen = screen
         self.board = Board()
         self.p0 = p0
         self.p1 = p1
-        self.items = [self.board,self.p0,self.p1]
+        self.ball = Ball(40,40,0,0)
+        self.items = [self.board,self.p0,self.p1,self.ball]
         # start game
         self.game()
 
