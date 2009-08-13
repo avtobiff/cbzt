00c393e28c47515db062cc7347ecb7533ee90101
Per Andersson <avtobiff@gmail.com>
2009-08-13 22:52:15 +0200

    added Drawable super class for drawable objects.

---
+++ cbzt/draw.py
@@ -0,0 +1,20 @@
+class Drawable(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.surface = None
+
+    def draw(self,screen):
+        screen.blit(self.surface,(self.x,self.y))
+
+    def get_surface(self):
+        return self.surface
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def update(self):
+        pass
