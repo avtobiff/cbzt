b5da1a47d20dba4109aa53c9b662ef08ceef4b10
Per Andersson <avtobiff@gmail.com>
2009-08-14 13:50:56 +0200

    added round handling.
    * cbzt/singleplayer.py (game): added break if someone wins, call to collision
      detector, and handling of collision.
      (gameover): new function, shows status of game on game over.
      (new_round): new function, reinits a new round for SINGLE PLAYER.

--- cbzt/singleplayer.py
+++ cbzt/singleplayer.py
@@ -58,10 +58,49 @@ class SinglePlayer(Game):
                 item.draw(self.screen)
                 pygame.display.flip()
 
-            # TODO: need collision detection
+            if self.ai.get_score() >= 9 or self.player.get_score() >= 9:
+                break
+
+            self.collision()
+
+            if self.left:
+                self.ai.do_score()
+                self.score1.set_score(self.ai.get_score())
+                self.new_round()
+                self.right = False
+            elif self.right:
+                self.player.do_score()
+                self.score0.set_score(self.player.get_score())
+                self.new_round()
+                self.right = False
+            elif self.top or self.bottom:
+                self.ball.bounce()
+                self.top, self.bottom = False, False
 
             self.read_input(pygame.event.poll())
             self.clock.tick(cbzt.FPS)
+
+        return self.gameover()
+
+    def gameover(self):
+        if self.ai.get_score() >= 9:
+            msg = "YOU LOST"
+        else:
+            msg = "YOU WON"
+        t = cbzt.text.render(msg,width=8,height=8)
+        sw,sh = t.get_size()
+        self.screen.blit(t,(320-sw/2,240-sh/2))
+        pygame.display.flip()
+
+        while 1:
+            self.read_input(pygame.event.poll())
+
+
+    def new_round(self):
+        self.ball.reinit(self.left)
+        self.player.reinit((40,240))
+        self.ai.reinit((600,240))
+        self.left, self.right = False, False
 
     def read_input(self,event):
         if event.type == pygame.QUIT:
