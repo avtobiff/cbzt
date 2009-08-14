90fd7d6c1c7e151155456097246f34daeb4b3a53
Per Andersson <avtobiff@gmail.com>
2009-08-14 11:42:00 +0200

    Drawable inherits from pygame.sprite.Sprite.
    * cbzt/draw.py (self.surface): renamed Drawable.surface to Drawable.image
      due to inheritance from Sprite.
      (self.rect): new variable, Sprite requires this.

--- cbzt/draw.py
+++ cbzt/draw.py
@@ -1,13 +1,19 @@
-class Drawable(object):
+import pygame
+
+
+class Drawable(pygame.sprite.Sprite):
     def __init__(self,surface,position):
+        pygame.sprite.Sprite.__init__(self)
         self.x, self.y = position
-        self.surface = surface
+        surface.convert()
+        self.image = surface
+        self.rect = self.image.get_rect()
 
     def draw(self,screen):
-        screen.blit(self.surface,(self.x,self.y))
+        screen.blit(self.image,(self.x,self.y))
 
     def get_surface(self):
-        return self.surface
+        return self.image
 
     def get_x(self):
         return self.x
