9a54c825900aa3ef6962be1596909ff85a07a1d8
Per Andersson <avtobiff@gmail.com>
2009-08-14 13:49:17 +0200

    added collision handling for SINGLE PLAYER.
    * cbzt/singleplayer.py (__init__): uses screen rect to handle ball out
      of court. introduced variables containing where ball exited court.
      (collision): new function, handles collision detection and ball out
      of court.

--- cbzt/singleplayer.py
+++ cbzt/singleplayer.py
@@ -12,10 +12,44 @@ from player import AI, Player
 class SinglePlayer(Game):
     def __init__(self,screen):
         pygame.key.set_repeat(1,50)
-        self.ball = Ball((80,240),random.random()*math.pi/2-random.random()*math.pi/2,5+random.random()*5)
+        self.area = screen.get_rect()
+        self.ball = Ball((80,240))
         self.player = Player(40)
         self.ai = AI(600,self.ball)
+        self.left, self.right = True, False # ball always starts on the left
+        self.top, self.bottom = False, False
         Game.__init__(self,screen,self.player,self.ai,self.ball)
+
+    def collision(self):
+        hit = False
+        if not self.area.contains(self.ball.rect):
+            self.left   = self.ball.get_x() < 5
+            self.right  = self.ball.get_x() > 635
+            self.top    = self.ball.get_y() < 5
+            self.bottom = self.ball.get_y() > 475
+
+
+        else: # collide with players
+            self.player.rect.inflate(-9,0)
+            self.ai.rect.inflate(-9,0)
+
+            if self.ball.rect.colliderect(self.player.rect) and not hit:
+                hit = True
+                x = self.player.get_x()-self.ball.get_x()
+                y = self.player.get_y()-self.ball.get_y()
+                velocity = math.sqrt(x**2+y**2)
+                direction = math.atan2(x,y)
+                self.ball.bounce(velocity,direction)
+            elif self.ball.rect.colliderect(self.ai.rect) and not hit:
+                hit = True
+                x = self.ai.get_x()-self.ball.get_x()
+                y = self.ai.get_y()-self.ball.get_y()
+                velocity = math.sqrt(x**2+y**2)
+                direction = math.atan2(x,y)
+                self.ball.bounce(velocity,direction)
+            elif hit:
+                self.hit = False
+
 
     def game(self):
         while 1:
